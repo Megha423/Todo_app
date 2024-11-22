df5b2f871b955ab965f0148375ffc7bca87baed7
Megha423 <meghasawant114@gmail.com>
2024-11-22 19:48:35 +0530

    Add files via upload

---
+++ todo_item.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/colors.dart';
+
+import '../../model/todo.dart';
+
+class ToDoItem extends StatelessWidget {
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
+
+  const ToDoItem({super.key, required this.todo, required this.onToDoChanged, required this.onDeleteItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {
+          onToDoChanged(todo);
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        tileColor: Colors.white,
+        leading: Icon(
+          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: tdBlack,
+        ),
+        title: Text(
+          todo.todoText!,
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: todo.isDone ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        trailing: Container(
+          padding: const EdgeInsets.all(0),
+          margin: const EdgeInsets.symmetric(vertical: 8),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdMauve,
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: IconButton(
+              color: tdBGColor,
+              iconSize: 18,
+              icon: const Icon(Icons.delete),
+            onPressed: () {
+                onDeleteItem(todo.id);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+}
